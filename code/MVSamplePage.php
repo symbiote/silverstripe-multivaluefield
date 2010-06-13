244fbb8a94ae4c7e731393f56deaa92b28473482
nyeholt <nyeholt@gmail.com>
2010-06-14 02:35:39 +1000

    TestOnly not working...??

--- code/MVSamplePage.php
+++ code/MVSamplePage.php
@@ -2,11 +2,9 @@
 /* All code covered by the BSD license located at http://silverstripe.org/bsd-license/ */
 
 /**
- * 
- *
  * @author Marcus Nyeholt <marcus@silverstripe.com.au>
  */
-class MVSamplePage extends Page implements TestOnly {
+class MVSamplePage extends Page {
     static $db = array(
 		'Ann' => 'MultiValueField',
 	);
