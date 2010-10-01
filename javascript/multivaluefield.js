e54339765abfbfe8b696df4a62de283cd8863a1f
nyeholt <nyeholt@gmail.com>
2010-10-01 15:20:34 +1000

    Fixed minor problem with val() returning null in multiselect listboxes

--- javascript/multivaluefield.js
+++ javascript/multivaluefield.js
@@ -5,7 +5,7 @@
 			// check to see if the one after us is there already - if so, we don't need a new one
 			var li = $(this).parent().next('li');
 			
-			if ($(this).val().length == 0) {
+			if ($(this).val() && $(this).val().length == 0) {
 				// lets also clean up if needbe
 				var nextText = li.find('input.mventryfield');
 				if (nextText && nextText.val() && nextText.val().length == 0) {
