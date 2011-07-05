de67ab9a625bf524447cf707f0d85f4cd8ac9fc4
Marcus Nyeholt <marcus@silverstripe.com.au>
2011-07-05 17:37:31 +1000

    Require livequery

--- code/fields/MultiValueDropdownField.php
+++ code/fields/MultiValueDropdownField.php
@@ -34,6 +34,7 @@ class MultiValueDropdownField extends MultiValueTextField {
 	}
 
 	public function Field() {
+		Requirements::javascript(THIRDPARTY_DIR.'/jquery-livequery/jquery.livequery.js');
 		Requirements::javascript('multivaluefield/javascript/multivaluefield.js');
 		
 		$name = $this->name . '[]';
