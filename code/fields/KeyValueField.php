dd2cf71dc8df5084285d9b3b272ef3171dc56ca9
nyeholt <nyeholt@gmail.com>
2012-01-06 16:19:54 +1100

    Hah - fixed spelling mistake

--- code/fields/KeyValueField.php
+++ code/fields/KeyValueField.php
@@ -30,7 +30,7 @@ class KeyValueField extends MultiValueTextField {
 					$fieldAttr = array(
 						'class' => 'mventryfield  mvkeyvalReadonly ' . ($this->extraClass() ? $this->extraClass() : ''),
 						'id' => $this->id().':'.$i,
-						'name' => $nakeKey,
+						'name' => $nameKey,
 						'tabindex' => $this->getTabIndex()
 					);
 
