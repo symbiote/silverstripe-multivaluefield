3efd76ff4f22c463e084714932e31e08850f8b71
Steve Boyd <emteknetnz@gmail.com>
2022-07-15 11:56:29 +1200

    MNT Use GitHub Actions CI

--- tests/MultiValueFieldTest_DataObject.php
+++ tests/MultiValueFieldTest_DataObject.php
@@ -6,11 +6,11 @@ namespace Symbiote\MultiValueField\Tests;
  */
 class MultiValueFieldTest_DataObject extends \SilverStripe\ORM\DataObject implements \SilverStripe\Dev\TestOnly
 {
+    private static $table_name = 'MultiValueFieldTest_DataObject';
 
     private static $db = array(
         'MVField' => 'MultiValueField'
     );
-
 
     public function write($showDebug = false, $forceInsert = false, $forceWrite = false, $writeComponents = false)
     {
