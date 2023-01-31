95b8ceadddef2a22a2fa903761bba6df22b20575
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2023-02-01 09:43:55 +1300

    DEP Upgrade frontend build stack (#88)

--- src/Fields/MultiValueDropdownField.php
+++ src/Fields/MultiValueDropdownField.php
@@ -45,11 +45,8 @@ class MultiValueDropdownField extends MultiValueTextField
 
     public function Field($properties = [])
     {
-        if (Controller::curr() instanceof ContentController) {
-            Requirements::javascript('silverstripe/admin: thirdparty/jquery/jquery.js');
-        }
-        Requirements::javascript('symbiote/silverstripe-multivaluefield: client/javascript/multivaluefield.js');
-        Requirements::css('symbiote/silverstripe-multivaluefield: client/css/multivaluefield.css');
+        Requirements::javascript('symbiote/silverstripe-multivaluefield: client/dist/js/multivaluefield.js');
+        Requirements::css('symbiote/silverstripe-multivaluefield: client/dist/styles/multivaluefield.css');
 
         $name   = $this->name.'[]';
         $fields = [];
